35e635e98ef5c284e487d50a2dc5b47f132520a8
Davi Chagas <davi.a.chagas@unesp.br>
2022-01-14 21:25:40 -0300

    Calculator does the desired operations and follows the rule of numerical expression

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const allClearButton = document.querySelector('.allClear')
 const deleteButton = document.querySelector('.delete')
 const equalButton = document.querySelector('.equal')
 const display = document.querySelector('.viewCurrentOperations')
+const history = document.querySelector('.viewPreviousOperations')
 
-let viewCurrentOperationContent = " "
-let result 
+let viewCurrentOperationContent = ""
+let calculation, result, newCalculation
+let operationsHistory = [], historyList
 
 viewableButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -23,17 +25,46 @@ viewableButtons.forEach(button => {
    })
 })
 
+
 allClearButton.addEventListener('click', () => {
     viewCurrentOperationContent = " "
+    operationsHistory = []
+
     display.textContent = viewCurrentOperationContent
+    history.innerHTML = " "
 })
+
 
 deleteButton.addEventListener('click', () => {
-    viewCurrentOperationContent = viewCurrentOperationContent.slice(0, viewCurrentOperationContent.length - 1)
-    display.textContent = viewCurrentOperationContent
+    if(viewCurrentOperationContent[viewCurrentOperationContent.length - 1] === ' ') {
+      viewCurrentOperationContent = viewCurrentOperationContent.slice(0, viewCurrentOperationContent.length - 2)
+    }
+
+    if(viewCurrentOperationContent[viewCurrentOperationContent.length - 1] !== ' ') {
+        viewCurrentOperationContent = viewCurrentOperationContent.slice(0, viewCurrentOperationContent.length - 1)
+}
+display.textContent = viewCurrentOperationContent
+
 })
 
+
 equalButton.addEventListener('click', () => {
+    calculation = viewCurrentOperationContent.split(' ')
+
+    calculation = calculation.map(item => item === '×' ? '*' : item);
+    calculation = calculation.map(item => item === '÷' ? '/' : item);
+    calculation = calculation.map(item => item === '–' ? '-' : item);
+
+    result = eval(calculation.join(' '))
+    display.textContent = result
+
+    operationsHistory.push(viewCurrentOperationContent)
+
+
+    historyList = operationsHistory.join("<li>", "</li>")
+    history.innerHTML = historyList
 
     
+    newCalculation = String(result)
+    viewCurrentOperationContent = newCalculation
 })
